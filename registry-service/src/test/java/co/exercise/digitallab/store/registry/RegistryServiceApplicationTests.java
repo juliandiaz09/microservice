96458683906eeee174a1bcc1f2342a50f4a89259
Julian Diaz <julian.diaz09@uptc.edu.co>
2024-01-14 13:44:22 -0500

    New files

---
+++ registry-service/src/test/java/co/exercise/digitallab/store/registry/RegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.exercise.digitallab.store.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
